42a9d8db66d6c3f8db7345b8e1e370907b1b8869
Andrew Stahl <andrewstahl96@gmail.com>
2022-03-29 21:35:06 -0400

    commit tree f5447339fe177eee85fb13f80da677416f63209d

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const mainElement = document.getElementById("main");
+mainElement.remove();
+
+const newHeader = document.createElement("h1");
+document.body.append(newHeader);
+
+newHeader.id = "victory";
+newHeader.textContent = "Andrew is the champion";
